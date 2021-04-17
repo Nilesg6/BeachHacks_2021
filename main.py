07f4746fb4ae25833efc0af09460b554696f0ca8
Nileshg6 <50922479+Nileshg6@users.noreply.github.com>
2021-04-17 13:19:02 -0700

    objects

--- main.py
+++ main.py
@@ -7,33 +7,42 @@ import yfinance as yf
 # from fbprophet import Prophet
 # from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
+from stockyahoo import Stock
 
 
 
 
 st.title('Stock App')
 selected_stock = st.text_input("Enter ticker:").upper()
+
+mainStock = Stock(selected_stock)
+sD = mainStock.getStockData()
+
+print(sD)
+
 # inter = st.text_input("Enter time:").upper()
 
-ticker = yf.Ticker(selected_stock)
 
-data_load_state = st.text('Loading data...')
-if(selected_stock != ""):
-    data = load_data(ticker)
-    # print(data)
-    # # if(data == None)
-    data_load_state.text('Loading data... done!')
 
-    st.write('$', selected_stock)
+# ticker = yf.Ticker(selected_stock)
 
-    # print(data)
-    # st.subheader('Raw data')
-    # st.write(data.tail())
+# data_load_state = st.text('Loading data...')
+# if(selected_stock != ""):
+#     data = stockyahoo.load_data(ticker)
+#     # print(data)
+#     # # if(data == None)
+#     data_load_state.text('Loading data... done!')
 
-    # Plot raw data
-    plot_raw_data(data)
+#     st.write('$', selected_stock)
 
-    if(st.button("1mo") == True):
-        inter='1mo'
-        data = load_data(ticker, inter)
-        plot_raw_data(data)
+#     # print(data)
+#     # st.subheader('Raw data')
+#     # st.write(data.tail())
+
+#     # Plot raw data
+#     stockyahoo.plot_raw_data(data)
+
+#     if(st.button("1mo") == True):
+#         inter='1mo'
+#         data = stockyahoo.load_data(ticker, inter)
+#         stockyahoo.plot_raw_data(data)
--- stockyahoo.py
+++ stockyahoo.py
@@ -9,49 +9,76 @@ import yfinance as yf
 # from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-st.title('Stock App')
-selected_stock = st.text_input("Enter ticker:").upper()
-# inter = st.text_input("Enter time:").upper()
+# st.title('Stock App')
+# selected_stock = st.text_input("Enter ticker:").upper()
+# # inter = st.text_input("Enter time:").upper()
 
-ticker = yf.Ticker(selected_stock)
+# ticker = yf.Ticker(selected_stock)
 
+#sy
+#ticker
+#data
 
-def load_data(ticker,interval = 'ytd'):
-    # print(interval)
-    data = ticker.history(period=interval)
-    data.reset_index(inplace=True)
-    return data
+#seperate
+#graph
+    #period
+    #ticker
+    #axes
+    #header
+    #
 
+class Stock():
 
-def plot_raw_data(data):
-    d = data
-    # fig = 0
-    # del fig
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=d['Date'], y=d['Open'], name="stock_open"))
-    fig.add_trace(go.Scatter(x=d['Date'], y=d['Close'], name="stock_close"))
-    fig.
-    # fig.add_trace(go.Scatter(x=data['Date'], y=data['Volume'], name="Volume"))
-    # fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+    ticker = []
+    stockData = []
 
-data_load_state = st.text('Loading data...')
-if(selected_stock != ""):
-    data = load_data(ticker)
-    # print(data)
-    # # if(data == None)
-    data_load_state.text('Loading data... done!')
+    def __init__(self, selected_stock):
+        ticker = yf.Ticker(selected_stock)
+        self.stockData = self.load_data(ticker)
 
-    st.write('$', selected_stock)
+    def load_data(self, ticker,interval = 'ytd'):
+        # print(interval)
+        data = ticker.history(period=interval)
+        data.reset_index(inplace=True)
+        return data
+        
+    def getStockData(self):
+        return self.stockData
 
-    # print(data)
-    # st.subheader('Raw data')
-    # st.write(data.tail())
+# class Graph():
+    
+#     def 
 
-    # Plot raw data
-    plot_raw_data(data)
+    # def plot_raw_data(data):
+    #     d = data
+    #     # fig = 0
+    #     # del fig
+    #     fig = go.Figure()
+    #     #scatter object class has list of all traces
+    #     fig.add_trace(go.Scatter(x=d['Date'], y=d['Open'], name="stock_open"))
+    #     fig.add_trace(go.Scatter(x=d['Date'], y=d['Close'], name="stock_close"))
+    #     fig.del()
+    #     # fig.add_trace(go.Scatter(x=data['Date'], y=data['Volume'], name="Volume"))
+    #     # fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    #     st.plotly_chart(fig)
 
-    if(st.button("1mo") == True):
-        inter='1mo'
-        data = load_data(ticker, inter)
-        plot_raw_data(data)
+# data_load_state = st.text('Loading data...')
+# if(selected_stock != ""):
+#     data = load_data(ticker)
+#     # print(data)
+#     # # if(data == None)
+#     data_load_state.text('Loading data... done!')
+
+#     st.write('$', selected_stock)
+
+#     # print(data)
+#     # st.subheader('Raw data')
+#     # st.write(data.tail())
+
+#     # Plot raw data
+#     plot_raw_data(data)
+
+#     if(st.button("1mo") == True):
+#         inter='1mo'
+#         data = load_data(ticker, inter)
+#         plot_raw_data(data)
