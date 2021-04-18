0a65eb8c869b12b0e3af64339011fc6a5c512d4b
bobdidley <james.austin@student.csulb.edu>
2021-04-17 20:54:31 -0700

    Calculator aesthetics

--- main.py
+++ main.py
@@ -11,21 +11,53 @@ from stockyahoo import Stock
 
 def run():
     options = ["Stock Look Up", "Calculator"]
-    choice = st.sidebar.selectbox("choose", options)
+    choice = st.sidebar.selectbox("choose", options, 1)
     if(choice == options[0]):
         stockLookUp()
     else:
         calculator()
 
 def calculator():
-    startDate = st.date_input("Enter Start Date", min_value = (datetime.datetime.now() - datetime.timedelta(days=1*365)), max_value = date.today())
-    endDate = st.date_input("Enter End Date", min_value = (datetime.datetime.now() - datetime.timedelta(days=1*365)), max_value = date.today()) 
+    st.title('Calculator')
+
+    selected_stock = st.text_input("Enter ticker:", "GME").upper()
+    share_amount = st.text_input("Enter share amount:", 0)
+    startDate = st.date_input("Enter Start Date", max_value = date.today())
+    endDate = st.date_input("Enter End Date", min_value = startDate, max_value = (startDate + datetime.timedelta(days=365))) 
+    # if (endDate - startDate).days <= 365:
+        # mainStock = Stock(selected_stock, start_date=startDate, end_date=endDate)
+        # mainStock.calculateCapitalGain()
+        # mainStock.graph()
+
+    # time = ["1d", "5d", "1mo", "ytd", "1y"]
+    # timePeriod = st.selectbox("Choose a demo", time, 4)
+
+    # debug
+    # print(timePeriod)
+    # diff = (endDate - startDate).days
+    # d = st.text_area('Capital Gain', value=diff)
+
+    st.markdown("""
+                <style>
+                .font-size {
+                    font-size:3vw;
+                }
+                </style>
+                """, unsafe_allow_html=True)
+
+    gain = Stock(selected_stock, startDate=startDate, endDate=endDate)
+    calc = gain.calculateCapitalGain(share_amount)
+    st.write('<p class="font-size">Capital Gain for ', share_amount, 'shares in ', selected_stock, ':', calc, '</p>', unsafe_allow_html=True)
+    # st.text()
+
+    # mainStock = Stock(selected_stock, timePeriod)
+    # mainStock.graph()
 
     
 
 def stockLookUp():
     st.title('Stock App')
-    selected_stock = st.text_input("Enter ticker:").upper()
+    selected_stock = st.text_input("Enter ticker:", "GME").upper()
 
     time = ["1d", "5d", "1mo", "ytd", "1y"]
 
